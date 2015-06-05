fbd530125b582a0a0b2646281fe3af13e3d8efc0
Ryan Davis <ryand-ruby@zenspider.com>
2015-06-04 17:00:29 -0700

    Make test runs work both inline in emacs and in batch mode.

--- test/enh-ruby-mode-test.el
+++ test/enh-ruby-mode-test.el
@@ -1,5 +1,7 @@
-(load-file "test/helper.el")
-(load-file "enh-ruby-mode.el")
+(load-file "helper.el")
+(load-file "../enh-ruby-mode.el")
+
+;; (ert-delete-all-tests)
 
 ;; In batch mode, face-attribute returns 'unspecified,
 ;; and it causes wrong-number-of-arguments errors.
@@ -12,7 +14,7 @@
 
 (ert-deftest enh-ruby-backward-sexp-test ()
   (with-temp-enh-rb-buffer
-   "test/rubytest-file.rb"
+   "rubytest-file.rb"
    (search-forward "def backward_sexp")
    (move-beginning-of-line nil)
    (enh-ruby-backward-sexp 1)
@@ -21,7 +23,7 @@
 (ert-deftest enh-ruby-backward-sexp-test-inner ()
   :expected-result :failed
   (with-temp-enh-rb-buffer
-   "test/rubytest-file.rb"
+   "rubytest-file.rb"
    (search-forward " word_")
    (move-end-of-line nil)
    (enh-ruby-backward-sexp 2)
@@ -29,7 +31,7 @@
 
 (ert-deftest enh-ruby-forward-sexp-test ()
   (with-temp-enh-rb-buffer
-   "test/rubytest-file.rb"
+   "rubytest-file.rb"
    (search-forward "def foo")
    (move-beginning-of-line nil)
    (enh-ruby-forward-sexp 1)
@@ -37,35 +39,35 @@
 
 (ert-deftest enh-ruby-up-sexp-test ()
   (with-temp-enh-rb-buffer
-   "test/rubytest-file.rb"
+   "rubytest-file.rb"
    (search-forward "test1_")
    (enh-ruby-up-sexp)
    (should (looking-at "def foo"))))
 
 (ert-deftest enh-ruby-indent-trailing-dots ()
   (with-temp-enh-rb-buffer
-   "test/rubytest-indent.rb"
+   "rubytest-indent.rb"
    (indent-region (point-min) (point-max))
    (search-forward "a.b.")
    (should (looking-at "\n  c"))))
 
 (ert-deftest enh-ruby-indent-leading-dots ()
   (with-temp-enh-rb-buffer
-   "test/rubytest-indent.rb"
+   "rubytest-indent.rb"
    (indent-region (point-min) (point-max))
    (search-forward "d.e")
    (should (looking-at "\n  .f"))))
 
 (ert-deftest enh-ruby-indent-pct-w-array ()
   (with-temp-enh-rb-buffer
-   "test/rubytest-indent.rb"
+   "rubytest-indent.rb"
    (indent-region (point-min) (point-max))
    (search-forward "words1 = %w[\n")
    (should (looking-at "          moo\n         ]\n"))))
 
 (ert-deftest enh-ruby-indent-array-of-strings ()
   (with-temp-enh-rb-buffer
-   "test/rubytest-indent.rb"
+   "rubytest-indent.rb"
    (indent-region (point-min) (point-max))
    (search-forward "words2 = [\n")
    (should (looking-at "          'moo'\n         ]\n"))))
